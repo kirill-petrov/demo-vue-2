da951818a8cd07e69ef2a20ddc039fd3b19e2ad1
kirill petrov <mail@kirill-petrov.ru>
2022-07-25 15:58:04 +0300

    testing forms

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <div> -->
+
+  <div id="123">
+    <template>
+      <label>{{ label }}</label>
+      <input
+        v-bind="$attrs"
+        :placeholder="label"
+        class="field"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </template>
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+};
+</script>
